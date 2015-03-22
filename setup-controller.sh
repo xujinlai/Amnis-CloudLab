dbb0b2034ed9846eb6bacf5409a538038d910fb9
David M. Johnson <johnsond@flux.utah.edu>
2015-03-22 03:20:17 -0600

    Change the openstack admin passwd to the ND one.

--- setup-controller.sh
+++ setup-controller.sh
@@ -127,6 +127,7 @@ if [ -z "${KEYSTONE_DBPASS}" ]; then
     # Create the admin user
     ADMIN_PASS=`$PSWDGEN`
     ADMIN_PASS=admin
+    ADMIN_PASS="N!ceD3m0"
     keystone user-create --name admin --pass ${ADMIN_PASS} --email "${SWAPPER_EMAIL}"
     # Create the admin role
     keystone role-create --name admin
