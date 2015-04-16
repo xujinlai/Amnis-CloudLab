6324156dc6cfa4db49c55f4d129cd6da0d2b5423
David M. Johnson <johnsond@flux.utah.edu>
2015-04-15 20:58:49 -0600

    Optimizations and features for aarch64 default VM image.
    We now have a prebuilt tarball that we download from boss, that
    has sshd, cloud-guest-utils, etc, preinstalled, to save time.  Use
    it by default.
    
    Also, create (at least) two images by default: trusty-server and
    trusty-server-multi-nic (which has support for up to 8 virtual NICs).
    Turns out Ubuntu distros have horrible support for "variable" NICs;
    if the NIC is listed in /etc/network/interfaces, the setup scripts
    will refuse to generate the right upstart event, which hangs the
    cloud-guest-utils init script for a long, long time.  So I've hacked
    a little service together that notes nonexistent NICs in
    /etc/network/interfaces and fires a fake complete event at the right
    time.  I didn't move this hack into the default image because I don't
    trust it enough.
    
    Also, create trusty-server first, so the first image is sane.

--- setup-basic-aarch64.sh
+++ setup-basic-aarch64.sh
@@ -30,13 +30,18 @@ fi
 echo "*** Building an ARM64 image ..."
 
 # need growpart
-apt-get update
-apt-get install cloud-guest-utils
+apt-get install -y cloud-guest-utils
 
 # Grab some files
 wget http://boss.utah.cloudlab.us/downloads/vmlinuz-3.13.0-40-arm64-generic
 wget http://boss.utah.cloudlab.us/downloads/initrd.img-3.13.0-40-arm64-generic
-wget http://boss.utah.cloudlab.us/downloads/ubuntu-core-14.04.1-core-arm64.tar.gz
+if [ ${BUILD_AARCH64_FROM_CORE} = 1 ]; then
+    wget http://boss.utah.cloudlab.us/downloads/ubuntu-core-14.04.1-core-arm64.tar.gz
+    core=ubuntu-core-14.04.1-core-arm64.tar.gz
+else
+    wget http://boss.utah.cloudlab.us/downloads/ubuntu-core-14.04.1-core-arm64-prebuilt.tar.gz
+    core=ubuntu-core-14.04.1-core-arm64-prebuilt.tar.gz
+fi
 
 echo "*** adding growpart to initrd ***"
 initrd=initrd.img-3.13.0-40-arm64-generic
@@ -85,7 +90,9 @@ done
 find . | cpio -o -H newc | gzip > ../$initrd
 cd ..
 
-core=ubuntu-core-14.04.1-core-arm64.tar.gz
+#
+# Build a 1GB image file to hold the tarball...
+#
 out=ubuntu-core-14.04.1-core-arm64.img
 
 dd if=/dev/zero of="$out" bs=1M count=1024
@@ -103,9 +110,17 @@ mount ${ld}p1 mnt
 echo "*** adding contents of core tarball ..."
 tar xzf "$core" -C mnt
 
-echo "*** adding ttyAMA0 ..."
-{
-    cat - <<EOM
+umount mnt
+
+#
+# If we didn't download it, build it from scratch.
+#
+if [ ${BUILD_AARCH64_FROM_CORE} = 1 ] ; then
+    mount ${ld}p1 mnt
+
+    echo "*** adding ttyAMA0 ..."
+    {
+	cat - <<EOM
 # ttyAMA0 - getty
 #
 # This service maintains a getty on ttyAMA0 from the point the system is
@@ -121,11 +136,11 @@ stop on runlevel [!2345]
 respawn
 exec /sbin/getty -L ttyAMA0 115200
 EOM
-} | tee mnt/etc/init/ttyAMA0.conf >/dev/null
+    } | tee mnt/etc/init/ttyAMA0.conf >/dev/null
 
-echo "*** adding NTP date and time synchronization ..."
-{
-    cat - <<EOM
+    echo "*** adding NTP date and time synchronization ..."
+    {
+	cat - <<EOM
 #
 # This task is run on startup to set the system date and time via NTP
 
@@ -136,66 +151,56 @@ start on startup
 task
 exec ntpdate -u ntp.ubuntu.com
 EOM
-} | tee mnt/etc/init/ntpdate.conf >/dev/null
+    } | tee mnt/etc/init/ntpdate.conf >/dev/null
 
-#
-# NOTE: we reduce the MTU arbitrarily here so that we can (easily) fit
-# through a GRE tunnel.  Also, add a second interface because our default
-# config has two interfaces -- a GRE tunnel data net, and a flat data "control"
-# net.
-#
-cp -p mnt/etc/network/interfaces ./etc.network.interfaces.ORIG
-echo "*** adding networking for qemu ..."
-{
-    cat - <<EOM
+    #
+    # NOTE: we reduce the MTU arbitrarily here so that we can (easily) fit
+    # through a GRE tunnel.  Also, add a second interface because our default
+    # config has two interfaces -- a GRE tunnel data net, and a flat data "control"
+    # net.
+    #
+    # NB: actually the MTU is reduced on the server side now.
+    #
+    cp -p mnt/etc/network/interfaces ./etc.network.interfaces.ORIG
+    echo "*** adding networking for qemu ..."
+    {
+	cat - <<EOM
 
 auto lo
 iface lo inet loopback
 
 auto eth0
 iface eth0 inet dhcp
-    post-up /sbin/ifconfig eth0 mtu 1400
-
-auto eth1
-iface eth1 inet dhcp
-    post-up /sbin/ifconfig eth1 mtu 1400
-
-auto eth2
-iface eth2 inet dhcp
-    post-up /sbin/ifconfig eth2 mtu 1400
 EOM
-} | tee -a mnt/etc/network/interfaces >/dev/null
+    } | tee -a mnt/etc/network/interfaces >/dev/null
 
+    echo 'Acquire::CompressionTypes::Order { "gz"; "bz2"; }' | tee mnt/etc/apt/apt.conf.d/99gzip >/dev/null
 
-echo 'Acquire::CompressionTypes::Order { "gz"; "bz2"; }' | tee mnt/etc/apt/apt.conf.d/99gzip >/dev/null
+    echo "*** fixing root password ..."
+    sed -i -e 's@root:[^:]*:@root:$6$QDmiL4Pp$OxXz9eP112jYY4rljT.1QUFqw.PW9g85VMapJehvRIDrkio1LN.74Tq40XbkvxCXAGEcLi.eZOaCFqgelSzOA/:@' mnt/etc/shadow
 
-echo "*** fixing root password ..."
-sed -i -e 's@root:[^:]*:@root:$6$QDmiL4Pp$OxXz9eP112jYY4rljT.1QUFqw.PW9g85VMapJehvRIDrkio1LN.74Tq40XbkvxCXAGEcLi.eZOaCFqgelSzOA/:@' mnt/etc/shadow
+    echo "*** fixing ubuntu password ..."
+    sed -i -e 's@ubuntu:[^:]*:@ubuntu:$6$QDmiL4Pp$OxXz9eP112jYY4rljT.1QUFqw.PW9g85VMapJehvRIDrkio1LN.74Tq40XbkvxCXAGEcLi.eZOaCFqgelSzOA/:@' mnt/etc/shadow
 
-echo "*** fixing ubuntu password ..."
-sed -i -e 's@ubuntu:[^:]*:@ubuntu:$6$QDmiL4Pp$OxXz9eP112jYY4rljT.1QUFqw.PW9g85VMapJehvRIDrkio1LN.74Tq40XbkvxCXAGEcLi.eZOaCFqgelSzOA/:@' mnt/etc/shadow
+    echo "*** unmounting ..."
+    umount mnt
 
-echo "*** unmounting ..."
-umount mnt
-rmdir mnt
+    # cp, upload later
+    cp -p $out $out.1
 
-# cp, upload later
-cp -p $out $out.1
+    #
+    # Now do another one, with sshd installed
+    #
+    mount ${ld}p1 mnt
 
-#
-# Now do another one, with sshd installed
-#
-mkdir -p mnt
-mount ${ld}p1 mnt
+    echo "*** installing ssh/sshd..."
+    echo "nameserver 8.8.8.8" > mnt/etc/resolv.conf
+    chroot mnt /usr/bin/apt-get update
+    chroot mnt /usr/bin/apt-get install -y openssh-server openssh-client
+    chroot mnt /usr/sbin/update-rc.d ssh defaults
+    chroot mnt /usr/sbin/update-rc.d ssh enable
 
-echo "*** installing ssh/sshd..."
-echo "nameserver 8.8.8.8" > mnt/etc/resolv.conf
-chroot mnt /usr/bin/apt-get update
-chroot mnt /usr/bin/apt-get install -y openssh-server openssh-client
-chroot mnt /usr/sbin/update-rc.d ssh defaults
-chroot mnt /usr/sbin/update-rc.d ssh enable
-
-cat <<EOF > mnt/etc/ssh/sshd_config
+    cat <<EOF > mnt/etc/ssh/sshd_config
 Port 22
 #ListenAddress ::
 #ListenAddress 0.0.0.0
@@ -240,50 +245,31 @@ PermitRootLogin yes
 PasswordAuthentication yes
 EOF
 
-echo "*** unmounting ..."
-umount mnt
+    echo "*** unmounting ..."
+    umount mnt
 
-cp -p $out $out.2
+    cp -p $out $out.2
 
-#
-# Now do another one, with cloud-init installed
-#
-mkdir -p mnt
-mount ${ld}p1 mnt
+    #
+    # Now do another one, with cloud-init installed
+    #
+    mount ${ld}p1 mnt
 
-echo "*** installing cloud-init and cloud-guest-utils..."
-chroot mnt /usr/bin/apt-get update
-chroot mnt /usr/bin/apt-get install -y cloud-guest-utils cloud-init
+    echo "*** installing cloud-init and cloud-guest-utils..."
+    chroot mnt /usr/bin/apt-get update
+    chroot mnt /usr/bin/apt-get install -y cloud-guest-utils cloud-init
 
-# permit root login!!
-sed -i -e 's/^disable_root: true$/disable_root: false/' mnt/etc/cloud/cloud.cfg
+    # permit root login!!
+    sed -i -e 's/^disable_root: true$/disable_root: false/' mnt/etc/cloud/cloud.cfg
 
-# don't overwrite the Ubuntu passwd we just hacked in
-sed -i -e 's/^.*lock_passwd:.*$/     lock_passwd: False/' mnt/etc/cloud/cloud.cfg
+    # don't overwrite the Ubuntu passwd we just hacked in
+    sed -i -e 's/^.*lock_passwd:.*$/     lock_passwd: False/' mnt/etc/cloud/cloud.cfg
 
-cp -p etc.network.interfaces mnt/etc/network/interfaces
+    echo "*** unmounting ..."
+    umount mnt
 
-cat <<EOF >>mnt/etc/network/interfaces
-auto lo
-iface lo inet loopback
-
-auto eth0
-iface eth0 inet dhcp
-
-auto eth1
-iface eth1 inet dhcp
-
-auto eth2
-iface eth2 inet dhcp
-EOF
-
-echo "*** unmounting ..."
-umount mnt
-rmdir mnt
-
-cp -p $out $out.3
-
-losetup -d ${ld}
+    cp -p $out $out.3
+fi
 
 echo "*** Importing kernel/ramdisk ..."
 
@@ -297,30 +283,106 @@ RAMDISK_ID=`glance image-show initrd-3.13.0-40-arm64-generic | grep id | sed -n 
 
 echo "*** Importing image with cloud-guest-utils  ..."
 
-glance image-create --name trusty-server --is-public True --progress --file $out.3 --disk-format ami --container-format ami
-
+glance image-create --name trusty-server --is-public True --progress --file $out --disk-format ami --container-format ami
 glance image-update --property kernel_args="console=ttyAMA0 root=/dev/sda" trusty-server
 glance image-update --property kernel_id=${KERNEL_ID} trusty-server
 glance image-update --property ramdisk_id=${RAMDISK_ID} trusty-server
 glance image-update --property root_device_name=/dev/vda1 trusty-server
 
-echo "*** Importing non-ssh image ..."
+echo "*** Creating image with cloud-guest-utils AND multi-nic support ..."
 
-glance image-create --name ubuntu-core-14.04.1-core-arm64 --is-public True --progress --file $out.1 --disk-format ami --container-format ami
+mount ${ld}p1 mnt
 
-glance image-update --property kernel_args="console=ttyAMA0 root=/dev/sda" ubuntu-core-14.04.1-core-arm64
-glance image-update --property kernel_id=${KERNEL_ID} ubuntu-core-14.04.1-core-arm64
-glance image-update --property ramdisk_id=${RAMDISK_ID} ubuntu-core-14.04.1-core-arm64
-glance image-update --property root_device_name=/dev/vda1 ubuntu-core-14.04.1-core-arm64
+#
+# Just blow it away -- the cloud guest utils handle this.
+# Well, actually, to add extra devices in there, we have to customize
+# the upstart config... anyway, give them 8 devices by default!
+#
+cat <<EOF >mnt/etc/network/interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+
+auto eth1
+iface eth1 inet dhcp
+
+auto eth2
+iface eth2 inet dhcp
+
+auto eth3
+iface eth3 inet dhcp
+
+auto eth4
+iface eth4 inet dhcp
+
+auto eth5
+iface eth5 inet dhcp
+
+auto eth6
+iface eth6 inet dhcp
+
+auto eth7
+iface eth7 inet dhcp
+EOF
+
+#
+# Ok, there is some delay with local-filesystems not finishing mounting
+# until after the cloud-utils have blocked up the system :(.  So, remove
+# this dependency.  You can see why the Ubuntu people have it in general,
+# but it will never bother us in this VM image!
+#
+#sed -i -e 's/^start on (local-filesystems$/start on (mounted MOUNTPOINT=\/ and mounted MOUNTPOINT=\/sys and mounted MOUNTPOINT=\/proc and mounted MOUNTPOINT=\/run/' mnt/etc/init/networking.conf
+sed -i -e 's/^start on (local-filesystems$/start on (mounted MOUNTPOINT=\//' mnt/etc/init/networking.conf
+
+#
+# Handle ethX devices that aren't actually in the VM instance by faking
+# that they are up so that startup doesn't get hung and timeout.  Our hack
+# only looks for ^eth\d*$ devices, and skips them if they're not accessible
+# via ifconfig.  This should be pretty safe, and it doesn't block things 
+# like bridges, ovs devices, tun/tap, etc.
+#
+cp -p $DIRNAME/etc/if-pre-up-upstart-faker mnt/etc/network/if-pre-up.d/
+
+echo "*** umounting..."
+
+umount mnt
+
+echo "*** Importing image with cloud-guest-utils AND multi-nic support ..."
+
+glance image-create --name trusty-server-multi-nic --is-public True --progress --file $out --disk-format ami --container-format ami
+glance image-update --property kernel_args="console=ttyAMA0 root=/dev/sda" trusty-server-multi-nic
+glance image-update --property kernel_id=${KERNEL_ID} trusty-server-multi-nic
+glance image-update --property ramdisk_id=${RAMDISK_ID} trusty-server-multi-nic
+glance image-update --property root_device_name=/dev/vda1 trusty-server-multi-nic
+
+if [ ${BUILD_AARCH64_FROM_CORE} = 1 ] ; then
+    echo "*** Importing non-ssh image ..."
+
+    glance image-create --name ubuntu-core-14.04.1-core-arm64 --is-public True --progress --file $out.1 --disk-format ami --container-format ami
+
+    glance image-update --property kernel_args="console=ttyAMA0 root=/dev/sda" ubuntu-core-14.04.1-core-arm64
+    glance image-update --property kernel_id=${KERNEL_ID} ubuntu-core-14.04.1-core-arm64
+    glance image-update --property ramdisk_id=${RAMDISK_ID} ubuntu-core-14.04.1-core-arm64
+    glance image-update --property root_device_name=/dev/vda1 ubuntu-core-14.04.1-core-arm64
+
+    echo "*** Importing image with sshd ..."
+
+    glance image-create --name ubuntu-core-14.04.1-core-arm64-sshd --is-public True --progress --file $out.2 --disk-format ami --container-format ami
+
+    glance image-update --property kernel_args="console=ttyAMA0 root=/dev/sda" ubuntu-core-14.04.1-core-arm64-sshd
+    glance image-update --property kernel_id=${KERNEL_ID} ubuntu-core-14.04.1-core-arm64-sshd
+    glance image-update --property ramdisk_id=${RAMDISK_ID} ubuntu-core-14.04.1-core-arm64-sshd
+    glance image-update --property root_device_name=/dev/vda1 ubuntu-core-14.04.1-core-arm64-sshd
+fi
+
+umount mnt
 rmdir mnt
-
-echo "*** Importing image with sshd ..."
-
-glance image-create --name ubuntu-core-14.04.1-core-arm64-sshd --is-public True --progress --file $out.2 --disk-format ami --container-format ami
-
-glance image-update --property kernel_args="console=ttyAMA0 root=/dev/sda" ubuntu-core-14.04.1-core-arm64-sshd
-glance image-update --property kernel_id=${KERNEL_ID} ubuntu-core-14.04.1-core-arm64-sshd
-glance image-update --property ramdisk_id=${RAMDISK_ID} ubuntu-core-14.04.1-core-arm64-sshd
-glance image-update --property root_device_name=/dev/vda1 ubuntu-core-14.04.1-core-arm64-sshd
+losetup -d ${ld}
 
 exit 0
