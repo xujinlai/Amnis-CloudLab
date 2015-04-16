8fa69a6adf87880f4c0900ed21a39fa6eec0e6f6
David M. Johnson <johnsond@flux.utah.edu>
2015-04-15 21:00:20 -0600

    Add a second VM image with support for multiple virtual NICs.

--- setup-basic-x86_64.sh
+++ setup-basic-x86_64.sh
@@ -32,19 +32,10 @@ echo "*** Downloading an x86_64 image ..."
 #wget https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img
 wget http://boss.apt.emulab.net/downloads/trusty-server-cloudimg-amd64-disk1.img
 
-echo "*** Modifying to support 3 NICs..."
-
 modprobe nbd max_part=8
 qemu-nbd --connect=/dev/nbd0 trusty-server-cloudimg-amd64-disk1.img
+
 mount /dev/nbd0p1 /mnt/
-cat <<EOF >> /etc/network/interfaces/eth1.cfg
-auto eth1
-iface eth1 inet dhcp
-EOF
-cat <<EOF >> /etc/network/interfaces/eth2.cfg
-auto eth2
-iface eth2 inet dhcp
-EOF
 
 echo "*** Setting up sshd ..."
 sed -i -e 's/^.*PermitRootLogin .*$/PermitRootLogin yes/' /mnt/etc/ssh/sshd_config
@@ -65,10 +56,72 @@ sed -i -e 's/^disable_root: true$/disable_root: false/' /mnt/etc/cloud/cloud.cfg
 sed -i -e 's/^.*lock_passwd:.*$/     lock_passwd: False/' /mnt/etc/cloud/cloud.cfg
 
 umount /mnt
-qemu-nbd -d /dev/nbd0
 
 echo "*** Importing new image ..."
 
 glance image-create --name trusty-server --is-public True  --disk-format qcow2 --container-format bare --progress --file trusty-server-cloudimg-amd64-disk1.img
+
+mount /dev/nbd0p1 /mnt/
+
+echo "*** Modifying to support up to 8 NICs..."
+
+cat <<EOF > /mnt/etc/network/interfaces
+# interfaces(5) file used by ifup(8) and ifdown(8)
+# Include files from /etc/network/interfaces.d:
+source-directory /etc/network/interfaces.d
+
+auto lo
+iface lo inet loopback
+
+auto eth0
+iface eth0 inet dhcp
+
+auto eth1
+iface eth1 inet dhcp
+
+auto eth2
+iface eth2 inet dhcp
+
+auto eth3
+iface eth3 inet dhcp
+
+auto eth4
+iface eth4 inet dhcp
+
+auto eth5
+iface eth5 inet dhcp
+
+auto eth6
+iface eth6 inet dhcp
+
+auto eth7
+iface eth7 inet dhcp
+EOF
+
+#
+# Ok, there is some delay with local-filesystems not finishing mounting
+# until after the cloud-utils have blocked up the system :(.  So, remove
+# this dependency.  You can see why the Ubuntu people have it in general,
+# but it will never bother us in this VM image!
+#
+#sed -i -e 's/^start on (local-filesystems$/start on (mounted MOUNTPOINT=\/ and mounted MOUNTPOINT=\/sys and mounted MOUNTPOINT=\/proc and mounted MOUNTPOINT=\/run/' mnt/etc/init/networking.conf
+sed -i -e 's/^start on (local-filesystems$/start on (mounted MOUNTPOINT=\//' /mnt/etc/init/networking.conf
+
+#
+# Handle ethX devices that aren't actually in the VM instance by faking
+# that they are up so that startup doesn't get hung and timeout.  Our hack
+# only looks for ^eth\d*$ devices, and skips them if they're not accessible
+# via ifconfig.  This should be pretty safe, and it doesn't block things 
+# like bridges, ovs devices, tun/tap, etc.
+#
+cp -p $DIRNAME/etc/if-pre-up-upstart-faker /mnt/etc/network/if-pre-up.d/
+
+umount /mnt
+
+echo "*** Importing new multi-nic image ..."
+
+glance image-create --name trusty-server-multi-nic --is-public True  --disk-format qcow2 --container-format bare --progress --file trusty-server-cloudimg-amd64-disk1.img
+
+qemu-nbd -d /dev/nbd0
 
 exit 0
