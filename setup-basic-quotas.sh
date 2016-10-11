1690632f9dcafb639de7a0078ce2117ec1995d4f
David M. Johnson <johnsond@flux.utah.edu>
2016-10-11 12:15:06 -0600

    Remove some duplicate code leftover from pre-parallelization stuff.

--- setup-basic-quotas.sh
+++ setup-basic-quotas.sh
@@ -33,29 +33,6 @@ if [ ${DEFAULT_SECGROUP_ENABLE_SSH_ICMP} -eq 1 ]; then
     nova secgroup-add-rule default tcp 22 22 0.0.0.0/0
 fi
 
-if [ $GENIUSER -eq 1 ] ; then
-    echo "*** Importing GENI user keys for admin user..."
-    $DIRNAME/setup-user-info.py
-
-    #
-    # XXX: ugh, this is ugly, but now that we have two admin users, we have
-    # to create keys for the admin user -- but we upload keys as the adminapi
-    # user.  I can't find a way with the API to upload keys for another user
-    # (seems very dumb, I must be missing something, but...)... so what we do
-    # is add the keys once for the adminapi user, change the db manually to
-    # make those keys be for the admin user, then add the same keys again (for
-    # the adminapi user).  Then both admin users have the keys.
-    #
-    AAID=`keystone user-get ${ADMIN_API} | awk '/ id / {print $4}'`
-    AID=`keystone user-get admin | awk '/ id / {print $4}'`
-    echo "update key_pairs set user_id='$AID' where user_id='$AAID'" \
-	| mysql -u root --password=${DB_ROOT_PASS} nova
-
-    # Ok, do it again!
-    echo "*** Importing GENI user keys, for ${ADMIN_API} user..."
-    $DIRNAME/setup-user-info.py
-fi
-
 if [ $QUOTASOFF -eq 1 ]; then
     nova quota-class-update --instances -1 default
     nova quota-class-update --cores -1 default
