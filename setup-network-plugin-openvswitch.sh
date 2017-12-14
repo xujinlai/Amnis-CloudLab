205a98022e119928020306f73d5ce6b382f54cfe
David M. Johnson <johnsond@flux.utah.edu>
2017-12-14 14:44:39 -0700

    Bug in 6b9863fce; restart correct service.

--- setup-network-plugin-openvswitch.sh
+++ setup-network-plugin-openvswitch.sh
@@ -266,7 +266,7 @@ if [ $OSVERSION -gt $OSLIBERTY ]; then
     # Restart the ovs agent one more time; something in its first-time
     # startup doesn't catch the reserved/preserved cookies, and ends up
     # wiping our flows.
-    service_restart neutron-ovs-cleanup
+    service_restart neutron-openvswitch-agent
     if [ -f /var/lib/neutron/ovs-default-flows.reserved_cookie -a -f /etc/neutron/ovs-default-flows/br-ex ]; then
 	cookie=`cat /var/lib/neutron/ovs-default-flows.reserved_cookie`
 	for fl in `cat /etc/neutron/ovs-default-flows/br-ex`; do
